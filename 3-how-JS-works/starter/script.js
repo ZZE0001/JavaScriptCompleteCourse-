597173fe7cbcf377390acba13d284296f2be8c80
Zane Zelenina <zane.zelenina@cgi.com>
2019-12-15 12:37:25 +0200

    hoisting for function declarations

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,21 +1,47 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+/**
+ * Hoisting - functions and variables are available before execution phase starts. 
+ * 
+ * Functions are already defined, but variables are undefined and 
+ * will be defined during execution phase.
+ * 
+ * Hoisting for function works only with function declarations, example below.
+ * 
+ * Most important use case for hoisting is that we can use function declarations before we actually declare them in code.
+ */
 
+/** FUNCTION HOSTING  */
+/**
+ * Hoisting works with FUNCTION DECLARATIONS
+ */
+calculateAge(1990);
 
+function calculateAge(year) {
+    console.log(2019 - year);
+}
+/*
+* Hoisting does not work with FUNCTION EXPRESSIONS
+*/
+retirement(1956);
 
+var retirement = function(year) {
+    console.log(65 - (2019 - year));
+}
 
+/** VARIABLES HOSTING  */
+console.log(age); //returns undefined - Javascript knows there is definition of varaible but does not know value yet
+var age = 20;
+console.log(age);
 
-
-
-
-
-
-
-
-
-
-
+function foo() {
+    console.log(age); // undefined
+    var age = 65;
+    console.log(age); // 65
+}
+foo();
+console.log(age); // 20
 
 ///////////////////////////////////////
 // Lecture: Scoping
