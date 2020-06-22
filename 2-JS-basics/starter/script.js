b589eb36faaff5a2028dd5f75260ba06069d7daa
Zane Zelenina <zane.zelenina@gmail.com>
2020-06-22 13:03:17 +0300

    CODING CHALLENGE 3 - Calculate Tips

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -15,7 +15,7 @@ GOOD LUCK 😀
 
 
 
-console.log('CODDING CHALLENGE 1 - Calculate BMIs');
+console.log('CODING CHALLENGE 1 - Calculate BMIs');
 
 // defines Marks and Johns parameters
 var massMark, heightMark, massJohn, heightMark;
@@ -60,17 +60,17 @@ while Mike's team scored 116, 94 and 123 points.
 GOOD LUCK 😀
 */
 
-console.log('CODDING CHALLENGE 2 - Average score for each basketball team');
+console.log('CODING CHALLENGE 2 - Average score for each basketball team');
 
 // calculates average for each team
-// johnsTeamScoreAverage   = (89+120+103)/3;
-// mikesTeamScoreAverage   = (116+94+123)/3;
-// mariasTeamScoreAverage  = (97+134+105)/3;
+johnsTeamScoreAverage   = (89+120+103)/3;
+mikesTeamScoreAverage   = (116+94+123)/3;
+mariasTeamScoreAverage  = (97+134+105)/3;
 
 // testing calculation with different scores, including draw scenario
-johnsTeamScoreAverage = (200+200+200)/3;
-mikesTeamScoreAverage = (200+200+200)/3;
-mariasTeamScoreAverage  = (200+200+200)/3;
+// johnsTeamScoreAverage = (200+200+200)/3;
+// mikesTeamScoreAverage = (200+200+200)/3;
+// mariasTeamScoreAverage  = (200+200+200)/3;
 
 // prints average for each team
 console.log(' John\'s team scores average '     + johnsTeamScoreAverage     + ' points \n',
@@ -113,3 +113,77 @@ switch (true){
     default:
         console.log('No answer found!');
 }
+
+/*****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. 
+The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). 
+He likes to tip 20% of the bill when the bill is less than $50, 
+15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 😀
+*/
+
+console.log('CODING CHALLENGE 3 - Calculate Tips');
+
+// variables
+let bill, tip;
+let bills = [124, 48, 268];
+let tips = [];
+let totals = [];
+
+// calculates tip and adds to array
+function calculateTip(bill){
+    
+    let percentage;
+
+    // calculates tip
+    if (bill < 50) {
+        // tip 20% of the bill when the bill is less than $50
+        percentage = .2;
+    } else if (bill > 200) {
+        // tip 10% if the bill is more than $200
+        percentage = .1;
+    } else {
+        // tip 15% when the bill is between $50 and $200
+        percentage = .15;
+    }
+    tip = bill * percentage;
+
+    // adds tip to array
+    tips.push(tip);
+
+    // calculates total and add to array
+    calculateTotal(bill, tip);
+
+}
+
+//calculates total (bill + tip) and adds to array
+function calculateTotal(bill, tip){
+    //calculate total
+    total = bill + tip;
+
+    //add total to array
+    totals.push(total);
+}
+
+// initializes tip calculation
+calculateTip(bills[0]);
+calculateTip(bills[1]);
+calculateTip(bills[2]);
+
+// prints all arrays
+console.log('Bills '    + bills);
+console.log('Tips '     + tips);
+console.log('Totals '    + totals);
